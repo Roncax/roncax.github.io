1dd82e2b5da79ae8a53dd066a3231788268e0659
paolo.roncaglioni <paolo.roncaglioni@gmail.com>
2023-12-08 02:01:52 +0100

    Updates

---
+++ static/js/84.5ea78634.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwebapp=self.webpackChunkwebapp||[]).push([[84],{7084:(e,s,a)=>{a.r(s),a.d(s,{default:()=>c});a(2791),a(3508);var t=a(184);const c=function(e){const{title:s}=e;return(0,t.jsx)("div",{className:"header",children:s})}}}]);
+//# sourceMappingURL=84.5ea78634.chunk.js.map
